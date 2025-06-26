1cd06e83646612c6c8afeded672fea5c61b049f1
Yasin-Ozdemir <yasinozdemirgs37@gmail.com>
2025-06-26 16:21:49 +0300

    Localization yapıldı, HomeViewModel ve PersonDetector daha dinamik hale getirildi.

---
+++ PersonDetector/Model/Detection.swift
@@ -0,0 +1,12 @@
+//
+//  Prediction.swift
+//  PersonDetector
+//
+//  Created by Yasin Özdemir on 26.06.2025.
+//
+
+import Foundation
+struct Prediction {
+    let score: Float
+    let rect: CGRect
+}
